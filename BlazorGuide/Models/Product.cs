66726e843e5b35ab2caa6184da60deb384094e5f
mujakzs <andriangultiano14@gmail.com>
2024-10-28 22:26:13 +0800

    Feat/BindProperties

---
+++ BlazorGuide/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorGuide.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+
+    public int Price { get; set; }
+    public bool IsActive { get; set; }
+}
