142cad543466db097ad9f293899b833f9f5372ec
mujakzs <andriangultiano14@gmail.com>
2024-10-29 23:00:21 +0800

    New/ProductList

--- BlazorGuide/Models/Product.cs
+++ BlazorGuide/Models/Product.cs
@@ -5,7 +5,7 @@ public class Product
     public int Id { get; set; }
     public string? Name { get; set; }
 
-    public int Price { get; set; }
+    public double Price { get; set; }
     public bool IsActive { get; set; }
 
     public IEnumerable<ProductProperties> ProductProperty { get; set; }
